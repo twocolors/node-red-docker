668e72c6b00abec7eb1cd564b9d710b655520c62
D <dmitry@yashin.su>
2022-09-08 17:27:26 +0300

    try docker build

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ echo "#########################################################################"
 echo "node-red version: ${NODE_RED_VERSION}"
 echo "#########################################################################"
 
+# linux/amd64, linux/arm64, linux/arm/v7, linux/arm/v6
+
 docker build --rm --no-cache \
-  --build-arg OS_ARCH=arm64v8 \
   --build-arg OS_NAME=bullseye-slim \
   --build-arg NODE_VERSION=16 \
-  --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
+  --build-arg TARGETPLATFORM="linux/amd64" \
   --file Dockerfile \
   --tag twocolors:node-red .
